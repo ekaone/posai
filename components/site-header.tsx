59ac7daf42dbc35a951878b2c73c84efc979f131
Eka Prasetia <ekaone3033@gmail.com>
2024-03-18 12:24:34 +0700

    Add cart icon with badge

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,48 +1,61 @@
-import Link from "next/link"
+import Link from 'next/link'
 
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
+import { siteConfig } from '@/config/site'
+import { buttonVariants } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
+import { Icons } from '@/components/icons'
+import { MainNav } from '@/components/main-nav'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+    <header className='sticky top-0 z-40 w-full border-b bg-background'>
+      <div className='container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
         <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
+        <div className='flex flex-1 items-center justify-end space-x-4'>
+          <nav className='flex items-center space-x-1'>
             <Link
               href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
+              target='_blank'
+              rel='noreferrer'
             >
               <div
                 className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
+                  size: 'icon',
+                  variant: 'ghost'
                 })}
               >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
+                <Icons.gitHub className='h-5 w-5' />
+                <span className='sr-only'>GitHub</span>
               </div>
             </Link>
             <Link
               href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
+              target='_blank'
+              rel='noreferrer'
             >
               <div
                 className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
+                  size: 'icon',
+                  variant: 'ghost'
                 })}
               >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
+                <Icons.twitter className='h-5 w-5 fill-current' />
+                <span className='sr-only'>Twitter</span>
               </div>
             </Link>
+            <div
+              className={buttonVariants({
+                size: 'icon',
+                variant: 'ghost'
+              })}
+            >
+              <Icons.cart className='h-5 w-5 fill-current' />
+              <span className='sr-only'>Cart</span>
+              <Badge className='relative left-[-7px] size-4 translate-y-[-10px] transform justify-center p-0 text-[10px]'>
+                5
+              </Badge>
+            </div>
             <ThemeToggle />
           </nav>
         </div>
